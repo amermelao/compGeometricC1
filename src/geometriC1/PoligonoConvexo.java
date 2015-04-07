1f02ca0deae4da044f268b973c8ffa44a5e97322
Francisca <faconcha@dcc.uchile.cl>
2015-04-07 17:54:45 -0300

    Agregue a PoligonoConvexo un metodo para obtener el vertice correspondiente a un indice

--- src/geometriC1/PoligonoConvexo.java
+++ src/geometriC1/PoligonoConvexo.java
@@ -9,6 +9,9 @@ public class PoligonoConvexo <E extends Number>{
 	
 	public List<AbstractPoint<E>> points = new ArrayList<AbstractPoint<E>>();
 	
-	
+	public AbstractPoint<E> getVertex(int i){
+		AbstractPoint<E> vertex = this.points.get(i);
+		return vertex;
+	}
 
 }
